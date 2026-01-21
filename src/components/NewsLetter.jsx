dc41ea23ce2b1634d8052677ffc477dc659ab993
abs01 <72456018+abs01@users.noreply.github.com>
2026-01-21 12:40:41 +0100

    Fix import path for CheckEmail utility

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -1,4 +1,4 @@
-import { CheckEmail } from "../utils/checkEmail";
+import { CheckEmail } from "../utils/checkEMail";
 import { useEffect, useState } from "react";
 import { BsNewspaper } from "react-icons/bs";
 import { AnimatedBackground } from "animated-backgrounds";
